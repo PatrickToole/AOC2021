8c2a9d8b1e50abd6ee729902c978eef668225427
patrick <toole.j.patrick@gmail.com>
2021-12-07 20:26:59 -0400

    day 2 part 2 solved. format part 1 better

--- solutions/day2.py
+++ solutions/day2.py
@@ -4,17 +4,49 @@ content = input_file.read()
 
 content_list = content.split('\n')
 input_file.close()
-    
-f_value = 0
-d_value = 0
 
-for i in content_list:
-    if 'forward' in i:
-        f_value += int(i[-1])
-    elif 'down' in i:
-        d_value += int(i[-1])
-    elif 'up' in i:
-        d_value -= int(i[-1])
 
-result = f_value * d_value
-print(result)
+# part 1
+
+
+def part_1(input):
+    f_value = 0
+    d_value = 0
+    for i in input:
+        if 'forward' in i:
+            f_value += int(i[-1])
+        elif 'down' in i:
+            d_value += int(i[-1])
+        elif 'up' in i:
+            d_value -= int(i[-1])
+
+    result = f_value * d_value
+    print(result)
+
+
+part_1(content_list)
+
+# part 2
+
+
+def part_2(input):
+    h_pos = 0
+    depth = 0
+    aim = 0
+
+    for i in input:
+        if 'forward' in i:
+            x = int(i[-1])
+            h_pos += x
+            depth += (x * aim)
+        elif 'down' in i:
+            y = int(i[-1])
+            aim += y
+        elif 'up' in i:
+            z = -int(i[-1])
+            aim += z
+    result = h_pos * depth
+    print(result)
+
+
+part_2(content_list)
